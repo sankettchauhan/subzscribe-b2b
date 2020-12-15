0f90f3268a831529372087bb6a41bed41628df29
sankettchauhan <sanket.chauhan4@gmail.com>
2020-12-15 16:45:10 +0530

    added copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,19 +7,20 @@ const Footer = () => {
       <div className="row no-gutters py-3">
         <div className="col-10 offset-1">
           <div>
-            <div>
-              <ul className="navbar-nav">
-                {LINKS.map((item, index) => (
-                  <li
-                    className="nav-item"
-                    key={"footer-link-" + item.name + "-" + { index }}>
-                    <a className="nav-link py-0" href={item.url}>
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <ul className="navbar-nav">
+              {LINKS.map((item, index) => (
+                <li
+                  className="nav-item"
+                  key={"footer-link-" + item.name + "-" + { index }}>
+                  <a className="nav-link py-0" href={item.url}>
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="text-center">
+            <small className="text-muted">&#169; Copyright 2020</small>
           </div>
         </div>
       </div>
